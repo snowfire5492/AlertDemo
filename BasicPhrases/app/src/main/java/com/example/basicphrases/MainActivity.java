9b3ffd5849b240f1f1450b068f52c7ab5c7f237f
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-06-14 07:22:58 -0700

    writing main function

--- BasicPhrases/app/src/main/java/com/example/basicphrases/MainActivity.java
+++ BasicPhrases/app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void speak(View view) {
+
+        Button pressedButton = (Button) view;
+
+        Log.i("pressed", pressedButton.getTag().toString());
+        
+        mediaPlayer = MediaPlayer.create(this,
+                getResources().getIdentifier(pressedButton.getTag().toString(), "raw", getPackageName()));
+        mediaPlayer.start();
+
     }
 }
