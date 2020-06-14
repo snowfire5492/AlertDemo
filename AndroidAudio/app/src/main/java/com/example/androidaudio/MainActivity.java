5c609308a36582e66064b00df3781d59478073e0
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-06-14 03:11:16 -0700

    create volume seekBar and location seekBar

--- AndroidAudio/app/src/main/java/com/example/androidaudio/MainActivity.java
+++ AndroidAudio/app/src/main/java/com/example/androidaudio/MainActivity.java
@@ -2,20 +2,90 @@ package com.example.androidaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mPlayer;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
         mPlayer = MediaPlayer.create(this, R.raw.alien_sound);
+
+        SeekBar volumeControl = findViewById(R.id.seekBar);
+
+
+
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(currentVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seekbar Changed", Integer.toString(i));
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar locationControl = findViewById(R.id.seekBarMove);
+
+        locationControl.setMax(mPlayer.getDuration());
+
+        locationControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Location SeekBar", Integer.toString(i));
+                mPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                locationControl.setProgress(mPlayer.getCurrentPosition());
+            }
+        }, 0, 300);
+
     }
 
     public void playMedia(View view) {
