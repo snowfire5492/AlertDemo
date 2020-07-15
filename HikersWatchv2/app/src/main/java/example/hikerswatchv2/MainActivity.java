13acd274a711392500ea476523521b7e1408fd7d
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-07-15 00:22:02 -0700

    got rid of action bar

---
+++ HikersWatchv2/app/src/main/java/example/hikerswatchv2/MainActivity.java
@@ -0,0 +1,14 @@
+package example.hikerswatchv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- UsersLocation/app/src/main/java/com/example/userslocation/MapsActivity.java
+++ UsersLocation/app/src/main/java/com/example/userslocation/MapsActivity.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,6 +24,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -79,6 +85,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Current Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (listAddresses != null && listAddresses.size() > 0){
+                        Log.i("LocationInfo", listAddresses.get(0).toString());
+
+                        String address = "";
+
+                        //address += listAddresses.get(0).getFeatureName() != null ? listAddresses.get(0).getFeatureName() : "";
+
+                        address += listAddresses.get(0).getThoroughfare();
+                        
+
+                        address += listAddresses.get(0).getAddressLine(0) != null ? listAddresses.get(0).getAddressLine(0): "";
+
+                        Log.i("AddressCheck", address);
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
 
